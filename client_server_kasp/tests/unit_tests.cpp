ba9cdaf072d77bfa0fa77de64379b698ba3df5e2
Matvey <mr.onv78@mail.ru>
2026-03-15 15:59:33 +0300

    добавлены простые тесты

--- client_server_kasp/tests/unit_tests.cpp
+++ client_server_kasp/tests/unit_tests.cpp
@@ -1,1 +1,58 @@
-//TODO: tests
+#include <gtest/gtest.h>
+#include "network/Protocol.h"
+#include <string>
+#include <thread>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+// совпадение отправленной и полученной длины
+TEST(LengthTest, FileSizeMatch) {
+    int sv[2];
+    ASSERT_EQ(socketpair(AF_UNIX, SOCK_STREAM, 0, sv), 0);
+
+    int serverFd = sv[0];
+    int clientFd = sv[1];
+
+    std::string content = "Hello World!";
+    uint32_t originalSize = content.length();
+    uint32_t networkSize = htonl(originalSize);
+
+    std::thread serverThread([serverFd, originalSize]() {
+        protocol::MessageHeader header;
+        recv(serverFd, &header.size, sizeof(header.size), 0);
+        uint32_t receivedSize = ntohl(header.size);
+        EXPECT_EQ(receivedSize, originalSize); 
+        close(serverFd);
+    });
+
+    send(clientFd, &networkSize, sizeof(networkSize), 0);
+    send(clientFd, content.c_str(), content.length(), 0);
+
+    close(clientFd);
+    serverThread.join();
+}
+
+// проверка длины для пустого файла
+TEST(LengthTest, ZeroLength) {
+    int sv[2];
+    ASSERT_EQ(socketpair(AF_UNIX, SOCK_STREAM, 0, sv), 0);
+
+    int serverFd = sv[0];
+    int clientFd = sv[1];
+
+    uint32_t networkZero = htonl(0);
+
+    std::thread serverThread([serverFd]() {
+        protocol::MessageHeader header;
+        recv(serverFd, &header.size, sizeof(header.size), 0);
+        uint32_t receivedSize = ntohl(header.size);
+        EXPECT_EQ(receivedSize, 0); 
+        close(serverFd);
+    });
+
+    send(clientFd, &networkZero, sizeof(networkZero), 0);
+
+    close(clientFd);
+    serverThread.join();
+}
