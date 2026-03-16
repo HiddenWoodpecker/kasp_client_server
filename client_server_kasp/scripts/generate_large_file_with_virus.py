163fe45175ecf4f8c1ccc70f99c2707aaafd3efd
Matvey <mr.onv78@mail.ru>
2026-03-16 11:18:59 +0300

    добавлен скрипт для генерации большого файла с вредоносными паттернами

---
+++ client_server_kasp/scripts/generate_large_file_with_virus.py
@@ -0,0 +1,23 @@
+import random
+import os
+
+# Вредоносные паттерны
+PATTERNS = [
+        "VIRUS",
+        "EVILSCRIPT",
+        "KEYLOGGER",
+        "CRYPTOMINER"
+    ]
+
+with open("large_file.txt", "w") as f:
+    size = 0
+    while size < 1024 * 15:
+        if random.random() < 0.03:
+            pattern = random.choice(PATTERNS)
+            f.write(pattern)
+            size += len(pattern)
+        else:
+            f.write("0")
+            size += 1
+
+print("Файл создан!")
